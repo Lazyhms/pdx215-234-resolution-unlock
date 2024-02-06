01db34bda79b2d3f5525f4b4ffe0a81fb667b29c
HMS <hu15150@126.com>
2024-02-06 09:50:24 +0800

    Update service.sh

--- magisk/module/service.sh
+++ magisk/module/service.sh
@@ -1,14 +1,9 @@
-# query WindowManagerService for mSystemBooted=true
-if [ "$(getprop ro.build.version.sdk)" = 33 ]
-then
+if [ "$(getprop ro.build.version.sdk)" ] <33; then
   exit 0
 fi
 
-count=0
-while let "count++ < 100"
-do
-  dumpsys window | grep -q mSystemBooted=true && break
-  sleep 1
+until [ $(resetprop sys.boot_completed) -eq 1 ]; do
+  sleep 0.5s
 done
 
 rate="$(settings get global user_preferred_refresh_rate)"
@@ -16,3 +11,11 @@ height="$(settings get global user_preferred_resolution_height)"
 width="$(settings get global user_preferred_resolution_width)"
 
 cmd display set-user-preferred-display-mode "$width" "$height" "$rate"
+
+density="$(wm density)"
+
+if [ width -eq 1644 ] && [ height -eq 3840] && [density -ne 534]; then
+  wm density 534
+elif [ width -eq 1096 ] && [ height -eq 2560] && [density -ne 356]; then
+  wm density 356
+fi
